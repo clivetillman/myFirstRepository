a4303a9c3d561a6e03e8a6c296a4d663e7bc1ca0
clivetillman <clivetillman@proton.me>
2025-07-20 11:19:33 +0100

    Second Python Program
    I added another python program to my repository

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+"""
+My First Python Program
+
+(This is a document string, called a "docstring",
+explaining the purpose of this file.)
+"""
+
+# Print a message saying hello!
+# (This is a comment, explaining the
+# following code.)
+print("Hello, World!")
+
